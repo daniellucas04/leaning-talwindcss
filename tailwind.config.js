833432942fffcdf98a29afebe5d43964add6e023
daniellucas04 <daniellsilva@outlook.com>
2022-11-02 20:35:22 -0300

    Header with tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+    theme: {
+    extend: {
+      colors: {
+        'dark-header': '#131313',
+        'white-header': '#999999',
+        'white-hover': '#E5E5E5',
+        'dark-hover': '#4c4c4c',
+      }
+    },
+  },
+  plugins: [],
+}
