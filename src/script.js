8656d8e4540725832017f009df40fb0752421935
daniellucas04 <daniellsilva@outlook.com>
2022-11-16 16:42:36 -0300

    Sending Email

--- src/script.js
+++ src/script.js
@@ -1,13 +1,14 @@
 const btnLogin = document.getElementById('btnLogin');
 const btnSignup = document.getElementById('btnSignup');
 
-function redirectLoginPage(){
+function redirectLoginPage() {
     location.href = '../src/pages/login.html';
 }
 
-function redirectSignupPage(){
+function redirectSignupPage() {
     location.href = '../src/pages/signup.html';
 }
 
 btnLogin.addEventListener('click', redirectLoginPage);
 btnSignup.addEventListener('click', redirectSignupPage);
+
